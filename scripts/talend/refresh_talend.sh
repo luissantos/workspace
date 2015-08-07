62c6f0a1a2d110da5180fb8e924204ad7bed3867
Luis Santos <luis@luissantos.pt>
2015-08-07 15:34:36 +0200

    - added script that removes "cache" jar files

---
+++ scripts/talend/refresh_talend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+TALEND_HOME="/Users/luissantos/Development/TOS_DI-20150702_1326-V6.0.0"
+
+rm -R -f ~/.m2/repository/org/talend/libraries/migration-lib-*
+
+rm -R -f $TALEND_HOME/workspace/.Java/lib/*.jar
+
+
